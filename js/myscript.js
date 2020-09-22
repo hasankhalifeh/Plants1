7e084fa8377368083b398765e44848840b819318
‘hasankhalifeh’ <hasan_khalifeh@Hotmail.com>
2020-09-22 19:42:08 +0300

    New touch

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+
+alert('Welcome to exotic plants');
+var age = ('18')
+age = prompt ('Please enter your age');
+console.log(age);
+
+if (condition){
+if (age < 18)
+        greeting = ('How did you find this website');
+} else if (age > 18){
+        greeting = ('Welcome to your plants website');
+}
+
+document.write('<h3>'+age+'</h3>');
+
+
+
+
+     
